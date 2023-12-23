e19d0356194d3251dc32cf62cdcac3427d8a3bf2
akshita2703 <rastogi32akshita@gmail.com>
2023-12-23 19:44:57 +0530

    Dynamic Programming

---
+++ LongestCommonPalindromicSubsequence/tabulation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        // Initialize a 2D array to store the lengths of LCS for subproblems
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Build the dp array bottom-up
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // The result is stored in the bottom-right cell of the dp array
+        return dp[m][n];
+    }
+    int longestPalindromeSubseq(string s) {
+        string t =s;
+        reverse(t.begin(),t.end());
+        return longestCommonSubsequence(s,t); 
+    }
+int main(){
+    
+    string text2= "madame";
+    cout << longestPalindromeSubseq(text2);
+}
